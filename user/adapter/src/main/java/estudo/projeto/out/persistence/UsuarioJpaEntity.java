b5fda3e4e51d0505b27d4ea2b64759fb7f87491b
Hércules da Silva Pereira <hercules.pereira@sptech.school>
2026-06-25 10:25:25 -0300

    feat: Adiciona UsuarioJpaEntity

---
+++ user/adapter/src/main/java/estudo/projeto/out/persistence/UsuarioJpaEntity.java
@@ -0,0 +1,56 @@
+package estudo.projeto.out.persistence;
+
+
+import estudo.projeto.entity.Cargo;
+import estudo.projeto.entity.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UsuarioJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Cargo cargo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "superior_id")
+    private UsuarioJpaEntity superior;
+
+    public Usuario toDomain(){
+        Usuario superiorDomain = this.superior != null ? this.superior.toDomain() : null;
+        return new Usuario(this.id, this.nome, this.email, this.cargo, superiorDomain);
+    }
+
+    public static UsuarioJpaEntity fromDomain(Usuario usuario){
+        if (usuario == null) return null;
+
+        UsuarioJpaEntity entity = new UsuarioJpaEntity();
+        entity.setId(usuario.getId());
+        entity.setNome(usuario.getNome());
+        entity.setEmail(usuario.getEmail());
+        entity.setCargo(usuario.getCargo());
+        entity.setSuperior(fromDomain(usuario.getSuperior()));
+
+        return entity;
+    }
+}
--- user/application/src/main/java/estudo/projeto/port/out/UsuarioFindPort.java
+++ user/application/src/main/java/estudo/projeto/port/out/UsuarioFindPort.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 public interface UsuarioFindPort {
     Optional<Usuario> findById(Long id);
     Page<Usuario> findAll(ListUsuariosQuery filtro, int pagina, int tamanho, String sortBy, String sortDirection);
-    boolean existsById(Long id);
     boolean existsByEmailIgnoreCaseAndIdNot(String email, Long id);
     boolean existsByEmailIgnoreCase(String email);
 }
